96b9f05a642674d63e842db1403407db821b13b4
Max Maschke <m.maschke@tu-bs.de>
2022-12-13 00:41:49 +0100

    Correctly (I hope) implemented statistics of squared position for random walks (Ex. 3)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # libs
 from numba import set_num_threads
-set_num_threads(10)
+# adjust this to your preference. at least one less than cpu cores is recommended,
+# since numba code can't easily be interrupted and you might lock up your system if
+# something goes wrong
+set_num_threads(10) 
 
 # code
 import randomwalk
@@ -9,10 +12,10 @@ import randomwalk_statistics
 
 
 def main():
-    #grid, coord_vec = randomwalk.self_avoiding_walk_protein(10, 30)
-    #randomwalk.plot_protein(coord_vec, 10, "data/protein_test_plot.pdf")
-    #energy.eigenvalue_statistics(1000000, "data/eigenvalue_distribution.pdf")
-    randomwalk_statistics.mean_sq_pos_stats_self_avoiding(10000, 20, 100, "data")
+    grid, coord_vec = randomwalk.self_avoiding_walk_protein(10, 30)
+    randomwalk.plot_protein(coord_vec, 10, "data/protein_test_plot.pdf")
+    energy.eigenvalue_statistics(1000000, "data/eigenvalue_distribution.pdf")
+    randomwalk_statistics.mean_sq_pos_stats_both(1000, 100, 100, "data")
 
 
 if __name__=="__main__":
--- randomwalk_statistics.py
+++ randomwalk_statistics.py
@@ -7,70 +7,114 @@ import matplotlib.pyplot as plt
 import randomwalk
 
 
-@njit
-def mean_sq_pos(coord_vec, steps):
-    x2_mean = 0.0
-    y2_mean = 0.0
-    for coords in coord_vec:
-        x2_mean += coords.x**2
-        y2_mean += coords.y**2
-    x2_mean /= steps
-    y2_mean /= steps
-
-    x2_std = 0.0
-    y2_std = 0.0
-    for coords in coord_vec:
-        x2_std += (coords.x**2 - x2_mean)**2
-        y2_std += (coords.y**2 - y2_mean)**2
-    mean_distance = np.sqrt(x2_mean + y2_mean)
-
-    return x2_mean, y2_mean, x2_std, y2_std, mean_distance
-
-
+# generate squared average positions for self-avoiding walk
+# note: walks may be discarded if they "curl up" prematurely!
+# another approach might be to let this run until runs is reached.
+# ^^^^ feel free to change!
 @njit(parallel=True)
 def sq_pos_stats_parallel_self_avoiding(runs, dim, steps):
-    xs              = np.zeros(runs*steps)
-    ys              = np.zeros(runs*steps)
-    x2_means        = np.zeros(runs, dtype=np.double)
-    y2_means        = np.zeros(runs, dtype=np.double)
-    x2_stds         = np.zeros(runs, dtype=np.double)
-    y2_stds         = np.zeros(runs, dtype=np.double)
-    mean_distances  = np.zeros(runs, dtype=np.double)
-    eff_runs = runs
+    x2s              = np.zeros(steps)
+    y2s              = np.zeros(steps)
+    eff_runs = runs # remove discarded runs
+
     for k in prange(runs):
         grid, coord_vec = randomwalk.self_avoiding_walk(dim, steps)
-        if len(coord_vec) != steps:
+        if coord_vec[-1].x == 0: # if last coord has coordinate 0, we have an invalid walk
             eff_runs -= 1
-            break
-        for i in range(k*steps, (k+1)*steps):
-            xs[i] = coord_vec[i % steps].x
-            ys[i] = coord_vec[i % steps].y
-        x2_mean, y2_mean, x2_std, y2_std, mean_distance = mean_sq_pos(coord_vec, steps)
-        x2_means[k]         = x2_mean
-        y2_means[k]         = y2_mean
-        x2_stds[k]          = x2_std
-        y2_stds[k]          = y2_std
-        mean_distances[k]   = mean_distance
-    return xs, ys, x2_means, y2_means, x2_stds, y2_stds, mean_distances, eff_runs
+            continue
+        for i in range(steps):
+            x2s[i] += coord_vec[i].x**2
+            y2s[i] += coord_vec[i].y**2
+        
+    return x2s/eff_runs, y2s/eff_runs, eff_runs # calculate mean correctly (divide by eff_runs)
 
 
+# generate squared average positions for random walk
+@njit(parallel=True)
+def sq_pos_stats_parallel(runs, dim, steps):
+    x2s              = np.zeros(steps)
+    y2s              = np.zeros(steps)
+
+    for k in prange(runs):
+        grid, coord_vec = randomwalk.random_walk(dim, steps)
+        for i in range(steps):
+            x2s[i] += coord_vec[i].x**2
+            y2s[i] += coord_vec[i].y**2
+        
+    return x2s/runs, y2s/runs
+
+
+
+# at this time, this "only" plots the distance from the origin as a function
+# of time = step for a self-avoiding walk
+# feel free to add cool stuff if you have ideas
+#
+# runs: # of walks to generate including ones that will potentially be discarded
+# dim: max. x-coordinate
+# steps: length of walks to be generated
+# path: where to save the picture
 def mean_sq_pos_stats_self_avoiding(runs, dim, steps, path):
-    xs, ys, x2_means, y2_means, x2_stds, y2_stds, mean_distances, eff_runs = sq_pos_stats_parallel_self_avoiding(runs, dim, steps)
+    x2s, y2s, eff_runs = sq_pos_stats_parallel_self_avoiding(runs, dim, steps)
+
+    distance = np.sqrt(x2s + y2s)
+    step = np.array([i for i in range(1, steps + 1)])
 
     fig, ax = plt.subplots()
-    ax.hist2d(x2_means, y2_means, bins= 100, range=[[0,np.sqrt(dim)], [0, np.sqrt(dim)]])
-    ax.set_ylim([0, np.sqrt(dim)])
-    ax.set_xlim([0, np.sqrt(dim)])
-    ax.set_aspect('equal')
-    ax.set_ylabel("$y^2$")
-    ax.set_xlabel("$x^2$")
-    ax.set_title("Mittlere quadratische Position")
-    fig.savefig(path + "/mean_sq_pos_self_av.pdf")
+    ax.plot(step, distance, label=f"Length $L$ = {steps}")
+    ax.set_xlabel("Time step $t$")
+    ax.set_ylabel("Mean distance of walk from origin $d$")
+    ax.set_title(f"Self-avoiding, {eff_runs} runs, {2*dim + 1}x{2*dim + 1}-grid")
+    ax.legend()
+    fig.savefig(path + "/mean_pos_avoid.pdf")
 
-    print(x2_means.mean(), y2_means.mean(), mean_distances.mean(), eff_runs)
+    print(eff_runs)
 
-    return fig,ax, xs, ys, x2_means, y2_means, x2_stds, y2_stds, mean_distances, eff_runs
+    return fig, ax, x2s, y2s, eff_runs
 
+
+# at this time, this "only" plots the distance from the origin as a function
+# of time = step for a random walk
+# feel free to add cool stuff if you have ideas
+#
+# runs: # of walks to generate
+# dim: max. x-coordinate
+# steps: length of walks to be generated
+# path: where to save the picture
+def mean_sq_pos_stats(runs, dim, steps, path):
+    x2s, y2s = sq_pos_stats_parallel(runs, dim, steps)
+
+    distance = np.sqrt(x2s + y2s)
+    step = np.array([i for i in range(1, steps + 1)])
+
+    fig, ax = plt.subplots()
+    ax.plot(step, distance, label=f"Length $L$ = {steps}")
+    ax.set_xlabel("Time step $t$")
+    ax.set_ylabel("Mean distance of walk from origin $d$")
+    ax.set_title(f"Non-self-avoiding, {runs} runs, {2*dim + 1}x{2*dim + 1}-grid")
+    ax.legend()
+    fig.savefig(path + "/mean_pos.pdf")
+
+
+    return fig, ax, x2s, y2s
+
+
+# like both the above but combined.
+def mean_sq_pos_stats_both(runs, dim, steps, path):
+    fig1, ax1, x2s_r, y2s_r = mean_sq_pos_stats(runs, dim, steps, path)
+    fig2, ax2, x2s_sa, y2s_sa, eff_runs = mean_sq_pos_stats_self_avoiding(runs, dim, steps, path)
+    distance_r = np.sqrt(x2s_r + y2s_r)
+    distance_sa = np.sqrt(x2s_sa + y2s_sa)
+    step = np.array([i for i in range(1, steps + 1)])
+
+    fig, ax = plt.subplots()
+    ax.plot(step, distance_r, label = f"non-avoiding, $n = {runs}$")
+    ax.plot(step, distance_sa, label = f"self-avoiding, $n = {eff_runs}$")
+    ax.set_xlabel("Time step $t$")
+    ax.set_ylabel("Mean distance of walk from origin $d$")
+    ax.set_title(f"{2*dim + 1}x{2*dim + 1}-grid")
+    ax.legend()
+
+    fig.savefig(path + "/mean_pos_comp.pdf")
 
 
 
